0a476b0ef7257a0f201f45dae8e6fa7a33c4ed02
Nigel Peris <nperis27@gmail.com>
2026-01-08 15:36:43 +0530

    fix: protect route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
-import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
