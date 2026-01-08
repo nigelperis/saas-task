2e69bc21c4807462fc87c46635e287928597bd07
Nigel Peris <nperis27@gmail.com>
2026-01-08 15:38:59 +0530

    fix: import

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
